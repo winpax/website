a2d05ff4dbc4c957f5ea587b5383d987983a78dc
Juliette Cordor <me@cordor.dev>
2025-01-16 15:03:36 +1100

    attempted implementation of suspenseful modal loading

--- src/app/_components/ProjectModal.tsx
+++ src/app/_components/ProjectModal.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
+import ProjectDisplay from './ProjectDisplay';
+import wrapPromise from '$/lib/wrapPromise';
+import { ProjectImport } from '$/lib/projects/metadata';
+
+function ProjectData({ project }: { project: string }) {
+	const projectData = wrapPromise<ProjectImport>(import(`$/lib/projects/${project}.mdx`)).read();
+
+	return <ProjectDisplay {...projectData} slug={project} />;
+}
 
 export default function ProjectModal({ project }: { project: string }) {
-	const ProjectPage = lazy(() => import('../projects/[slug]/page'));
-
 	return (
 		<dialog open className="card m-5 min-w-[50vw] max-w-[50vw] bg-base-100 shadow-xl">
 			<Suspense fallback={<div>Loading...</div>}>
-				<ProjectPage params={Promise.resolve({ slug: project })} />
+				<ProjectData project={project} />
 			</Suspense>
 		</dialog>
 	);
