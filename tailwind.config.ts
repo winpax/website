08b1c7c80f6cfd82104c74b5c96bc51fe7a767bf
Juliette Cordor <me@cordor.dev>
2025-12-08 17:59:54 +1100

    removed old daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
 import type { Config } from 'tailwindcss';
-// import daisyui, { type Config as DaisyConfig } from 'daisyui';
-import { darkTheme, themes } from './src/lib/themes';
 import typography from '@tailwindcss/typography';
 
 export default {
@@ -21,11 +19,6 @@ export default {
 			}
 		}
 	},
-	plugins: [typography /* daisyui */],
+	plugins: [typography],
 	darkMode: 'selector'
-
-	// daisyui: {
-	// 	themes,
-	// 	darkTheme
-	// } satisfies DaisyConfig
 } satisfies Config;
