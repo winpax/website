71929a02a2696d2271a1d02ec67f85f6aed7f7a7
Juliette Cordor <me@cordor.dev>
2025-12-08 18:23:39 +1100

    updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,10 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import { FlatCompat } from '@eslint/eslintrc';
 import reactCompiler from 'eslint-plugin-react-compiler';
 import nextVitals from 'eslint-config-next/core-web-vitals';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
-
-const config = [...compat.extends('next/core-web-vitals', 'next/typescript')];
 
 export default defineConfig(
 	js.configs.recommended,
@@ -39,6 +26,5 @@ export default defineConfig(
 			}
 		}
 	},
-	...config,
 	reactCompiler.configs.recommended
 );
